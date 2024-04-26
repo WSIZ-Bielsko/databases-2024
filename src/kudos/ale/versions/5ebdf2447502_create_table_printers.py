e6df175d5a3b031f9d839fdca7eaa59648c04c17
Piotr Marecki <pmarecki@gmail.com>
2024-04-26 17:05:44 +0200

    feat: enable alembic; add 2 migrations

---
+++ src/kudos/ale/versions/5ebdf2447502_create_table_printers.py
@@ -0,0 +1,34 @@
+"""create table kudo
+
+Revision ID: 5ebdf2447502
+Revises: 
+Create Date: 2024-04-26 16:41:20.555468
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '5ebdf2447502'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("""
+    create table printers(
+      id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+      ptype text,
+      descrition text 
+    );
+    """)
+
+
+def downgrade() -> None:
+    op.execute("""
+    drop table if exists printers;
+    """)
---
+++ src/kudos/ale/versions/6445ee0eb703_add_column_printed_pages_to_printers.py
@@ -0,0 +1,30 @@
+"""add column printed_pages to printers
+
+Revision ID: 6445ee0eb703
+Revises: 5ebdf2447502
+Create Date: 2024-04-26 17:02:31.382902
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6445ee0eb703'
+down_revision: Union[str, None] = '5ebdf2447502'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("""
+    alter table printers add column printed_pages int default 0;
+    """)
+
+
+def downgrade() -> None:
+    op.execute("""
+    alter table printers drop column printed_pages;
+    """)
